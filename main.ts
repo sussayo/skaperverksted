24fec3d161a64ab3643f2c079edfb7ae001c8d74
sussayo <125973962+sussayo@users.noreply.github.com>
2023-02-23 11:07:56 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,23 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
-    radio.sendNumber(1)
+    radio.sendNumber(2)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
     basic.clearScreen()
 })
+let reading = 0
 basic.showString("god morgen")
 radio.setGroup(1)
+basic.forever(function () {
+    reading = pins.analogReadPin(AnalogPin.P0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (true) {
+        basic.showNumber(reading)
+    }
+    radio.sendNumber(3)
+})
